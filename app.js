96637e22c4612347e3d1f4a8d0dc0d80bc875e48
Elana Kopelevich <ekopelevich@gmail.com>
2016-10-10 15:00:02 -0600

    start twitter passport setup

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 'use strict'
 
+require('dotenv').config()
+
 const express = require('express')
 const path = require('path')
 const logger = require('morgan')
@@ -8,6 +10,10 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 // const fortune = require('fortune')
 const index = require('./routes/index')
+const session = require('express-session')
+const passport = require('passport')
+const Strategy = require('passport-twitter').Strategy
+// const auth = require('./routes/auth')
 const app = express()
 
 app.use(cors())
@@ -16,6 +22,45 @@ app.use(bodyParser.json({ type: 'application/*+json' }))
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
+
+passport.use(new Strategy({
+    consumerKey: process.env.TWITTER_CONSUMER_KEY,
+    consumerSecret: process.env.TWITTER_CONSUMER_SECRET,
+    callbackURL: 'http://127.0.0.1:3000/auth/twitter/callback',
+  },
+  function(token, tokenSecret, profile, cb) {
+    cb(null, {id: profile.id, displayName: profile.displayName})
+  })
+)
+
+app.use(session({secret: 'secret', saveUninitialized: true, resave: true}))
+app.use(passport.initialize())
+app.use(passport.session())
+
+passport.serializeUser(function(user, done) {
+  done(null, user)
+})
+
+passport.deserializeUser(function(user, done) {
+  done(null, user)
+})
+
+app.get('/auth/twitter',
+  passport.authenticate('twitter'),
+  function(req, res){
+    // The request will be redirected to Twitter for authentication,
+    // so this function will not be called.
+    res.send('This function shouldn\'t have been called')
+  })
+
+app.get('/auth/twitter/callback', passport.authenticate('twitter', {
+  successRedirect: '/',
+  failureRedirect: '/login',
+}))
+
+app.get('/logout', function(req, res){
+  res.redirect('/')
+})
 
 app.use('/api/v1', index)
 
