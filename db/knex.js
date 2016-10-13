a9fe9a8206dcc4c4fe6a9c0d424042564f7df095
Elana Kopelevich <ekopelevich@gmail.com>
2016-10-13 11:07:33 -0600

    remove semicolons in knex config

--- db/knex.js
+++ db/knex.js
@@ -1,5 +1,5 @@
-var environment = process.env.NODE_ENV || 'development';
-var config = require('../knexfile');
-var knex = require('knex')(config[environment]);
+var environment = process.env.NODE_ENV || 'development'
+var config = require('../knexfile')
+var knex = require('knex')(config[environment])
 
-module.exports = knex;
+module.exports = knex
