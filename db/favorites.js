9fe9a5eb5436b2f1d9b171ee1bd626290e15fff0
Elana Kopelevich <ekopelevich@gmail.com>
2016-10-25 14:31:38 -0600

    add and remove favorite record

--- db/favorites.js
+++ db/favorites.js
@@ -4,14 +4,15 @@ var knex = require('./knex')
 
 module.exports = {
   setFavorite(data){
-    console.log('data', data)
     return knex('favorites')
-    .where('user_id', data.user_id).andWhere('story_id', data.story_id)
+    .where('user_id', data.user_id).andWhere('story_id', data.story_id).first()
     .then(record => {
+      console.log('record', record)
       if (record) {
-        return knex('favorites').where('id', record.id)
-        .update({user_id: data.user_id, story_id: data.story_id}, 'id')
+        console.log('gonna remove a fav record')
+        return knex('favorites').where('id', record.id).first().del()
       } else {
+        console.log('gonna make a fav record', data)
         return knex('favorites')
         .insert({user_id: data.user_id, story_id: data.story_id})
       }
--- routes/favorites.js
+++ routes/favorites.js
@@ -5,8 +5,7 @@ const router = express.Router()
 const db = require('../db/favorites')
 
 router.post('/', function(req, res) {
-  console.log(req.body)
-  db.setFavorite(req.body).then(favId => res.send(favId))
+  db.setFavorite(req.body).then(id => res.send({id:id}))
 })
 
 module.exports = router
