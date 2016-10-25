f1df76878843d2fc05f1619939ff3b690f074464
Elana Kopelevich <ekopelevich@gmail.com>
2016-10-25 14:32:49 -0600

    remove console logs from favorites db api

--- db/favorites.js
+++ db/favorites.js
@@ -7,12 +7,9 @@ module.exports = {
     return knex('favorites')
     .where('user_id', data.user_id).andWhere('story_id', data.story_id).first()
     .then(record => {
-      console.log('record', record)
       if (record) {
-        console.log('gonna remove a fav record')
         return knex('favorites').where('id', record.id).first().del()
       } else {
-        console.log('gonna make a fav record', data)
         return knex('favorites')
         .insert({user_id: data.user_id, story_id: data.story_id})
       }
