9f59733a5d9bcbe6bcc91d9f2f0b2a4ab83ccbe4
Elana Kopelevich <ekopelevich@gmail.com>
2016-11-30 13:22:40 -0700

    env var for server host

--- routes/auth.js
+++ routes/auth.js
@@ -20,12 +20,13 @@ router.get('/twitter', passport.authenticate('twitter'))
 // This route receives the auth 'code' from Twitter
 // The cb makes 2 api calls to Twitter to compare user credentials and get user info back
 router.get('/twitter/callback', passport.authenticate('twitter', {
-  successRedirect: '/api/v1/auth/success',
+  successRedirect: `${process.env.SERVER_HOST}/api/v1/auth/success`,
   failureRedirect: process.env.CLIENT_HOST,
 }))
 
 router.get('/success', (req, res) => {
-  res.redirect(`${process.env.CLIENT_HOST}/#/users/${req.user.id}/profile`)
+  console.log('success')
+  res.redirect(`${process.env.CLIENT_HOST}/users/${req.user.id}/profile`)
 })
 
 router.get('/logout', (req, res) => {
